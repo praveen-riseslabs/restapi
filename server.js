29f850712e2e9bacc78bf9715cc0a7cea9459dd1
Praveen Rondi <praveen.rondi@idera.com>
2024-03-05 14:59:29 +0530

    added

--- server.js
+++ server.js
@@ -39,16 +39,15 @@ mongoose.Promise = global.Promise;
 // listen for requests
 
 mongoose.set("bufferCommands", false);
-
+const options = {
+  autoIndex: false, // Don't build indexes
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4 // Use IPv4, skip trying IPv6
+};
 const connect = mongoose
-  .connect(DB.url, {
-    // useNewUrlParser: true
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: true,
-    // tlsCAFile: mongoCertPath, ----> you should remove this line
-    socketTimeoutMS:43200000,
-  })
+  .connect(DB.url,options)
   .then(() => {
     console.log("Successfully connected to the database");
   })
