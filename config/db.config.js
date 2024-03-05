f8ef29445c590606bc72ab90c3f2ee60206b2320
Praveen Rondi <praveen.rondi@idera.com>
2024-03-05 14:50:31 +0530

    code added

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 const DB = {
-    url:'mongodb://ec2-13-127-120-169.ap-south-1.compute.amazonaws.com:27017/myfacebook' 
+    url:'mongodb://ec2-13-127-120-169.ap-south-1.compute.amazonaws.com:27017/myfacebook'
 }
 
 export default DB;
--- server.js
+++ server.js
@@ -10,14 +10,12 @@ import forgotRouter from "./routes/forgot.router.js";
 import resetRouter from "./routes/reset.router.js";
 import employeeRouter from "./routes/employee.route.js";
 
-
 //const mongoose = require("mongoose");
 
 //const bodyParser = require("body-parser");
 // create express app
 
-
-const app = express(); 
+const app = express();
 // Setup server port
 
 const port = process.env.PORT || 4000;
@@ -28,25 +26,11 @@ app.use(bodyParser.json());
 // Configuring the database
 mongoose.Promise = global.Promise;
 // Connecting to the database
-// mongoose.connect(DB.url, {
-//     useNewUrlParser: true
-//   })
-//   .then(() => {
-//     console.log("Successfully connected to the database");
-//   })
-//   .catch(err => {
-//     console.log("Could not connect to the database.", err);
-//     process.exit();
-//   });
-
-  
-
 
 // Require Users routes
 //const userRoutes = require("./routes/user.routes");
 //const loginRoutes = require("./routes/login.routes");
 // using as middleware
-
 
 // define a root/default route
 // app.get("/", (req, res) => {
@@ -54,44 +38,25 @@ mongoose.Promise = global.Promise;
 // });
 // listen for requests
 
-const start = async () => {
+mongoose.set("bufferCommands", false);
 
- mongoose.set("bufferCommands", false);
-  if (!DB.url) {
-      throw new Error('auth DB_URI must be defined');
-  }
-  try {
-      await mongoose.connect(DB.url, {
-          useNewUrlParser: true,
-          socketTimeoutMS: 0,
-          connectTimeoutMS: 2000,
-          //useCreateIndex: true,
-          
-          useUnifiedTopology: true,
-      });
-      // const connection =  mongoose.connection;
-      // connection.once("open", () => {
-      // console.log("MongoDB database connection established successfully");
-    // });
-  } catch (err) {
-      //throw new DbConnectionError();
-      console.error(err);
-  }
-
-
-  app.listen(port, () => {
-    console.log(`Node server is listening on port ${port}`);
+const connect = mongoose
+  .connect(DB.url, {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch(err => {
+    console.log("Could not connect to the database.", err);
+    process.exit();
   });
-  setTimeout(() => {
-      app.use("/api/v1", userRoutes);
-      app.use("/api/v1/user", loginRouter);
-      app.use("/api/v1/forgot", forgotRouter);
-      app.use("/api/v1/reset", resetRouter);
-      app.use("/api/v1/emp", employeeRouter);
-  }, 30000)
-};
-
-
-start();
-
-
+app.use("/api/v1", userRoutes);
+app.use("/api/v1/user", loginRouter);
+app.use("/api/v1/forgot", forgotRouter);
+app.use("/api/v1/reset", resetRouter);
+app.use("/api/v1/emp", employeeRouter);
+app.listen(port, async () => {
+  await connect;
+  console.log(`Node server is listening on port ${port}`);
+});
