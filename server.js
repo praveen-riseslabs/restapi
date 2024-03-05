0a8316a733ac85a65a2d20cd6296eba92cd4893f
Praveen Rondi <praveen.rondi@idera.com>
2024-03-05 15:24:48 +0530

    config added

--- server.js
+++ server.js
@@ -40,11 +40,8 @@ mongoose.Promise = global.Promise;
 
 mongoose.set("bufferCommands", false);
 const options = {
-  autoIndex: false, // Don't build indexes
-  maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4 // Use IPv4, skip trying IPv6
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 };
 const connect = await mongoose
   .connect(DB.url,options)
